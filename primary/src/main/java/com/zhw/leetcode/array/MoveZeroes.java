03af62360b6e2b1d3db129209ae80343b11b4711
mlskcq <252297092@qq.com>
2018-09-19 11:53:05 +0800

    Update MoveZeroes.java

--- primary/src/main/java/com/zhw/leetcode/array/MoveZeroes.java
+++ primary/src/main/java/com/zhw/leetcode/array/MoveZeroes.java
@@ -40,3 +40,16 @@ public class MoveZeroes {
         }
     }
 }
+
+
+// public void moveZeroes2(int[] nums) {
+//         int zeroNum=0;//用来计算0的个数
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]==0){//如果nums[i]=0,zeroNum个数加一
+//                 zeroNum++;
+//             }else if(zeroNum!=0){//如果nums[i]不等于0,将nums[i]跟前zeroNum个交换，并且将nums[i]赋值为0
+//                 nums[i-zeroNum]=nums[i];  
+//                 nums[i]=0;
+//             }
+//         }
+//     }
